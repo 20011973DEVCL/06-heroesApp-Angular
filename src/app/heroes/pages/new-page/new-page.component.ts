bd63e71a3075e90945bbddb8257671b165daf627
Danilo Alarcon Lopez <danilo.alarcon.lopez@icloud.com>
2023-09-11 16:34:48 -0300

    lazy load with children paths

---
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'new-hero',
+  templateUrl: './new-page.component.html',
+  styles: [
+  ]
+})
+export class NewPageComponent {
+
+}
